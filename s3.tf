87171292c4b3c96972e563034cf8b4f4aa71184e
Felix-anand <101624641+Felix-anand@users.noreply.github.com>
2022-04-06 12:36:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "my_bucket" {
 
 resource "aws_s3_bucket_object" "file_upload" {
   bucket = "anand109"
-  key    = "vars.tf"
-  source = "${path.module}/vars.tf"
-  etag   = "${filemd5("${path.module}/vars.tf")}"
+  key    = "vpc.tf"
+  source = "${path.module}/vpc.tf"
+  etag   = "${filemd5("${path.module}/vpc.tf")}"
 }
